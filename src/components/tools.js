1593b49819edcbc9635107946186954b6618c5d9
BLewis <45450044+BLewis-Keyin@users.noreply.github.com>
2024-01-21 23:53:19 -0500

    Modify API endpoint

--- src/components/tools.js
+++ src/components/tools.js
@@ -9,7 +9,7 @@ function Tools() {
         e.preventDefault();
         setIsLoading(true);
         try {
-            const response = await fetch('https://RedditVideoScaperv-env.eba-vnx8q8n6.us-west-2.elasticbeanstalk.com/process_video', {
+            const response = await fetch('https://rvtool.blcode.net/process_video', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
