a06f95b9694f2d1f4878f796d803bb8667f6f102
BL-Keyin <you@example.com>
2024-03-13 07:02:49 -0400

    Portfolio Update 3/13/2024
    fixed some CSS and linking to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,20 +6,20 @@ function Projects() {
         <section className="projects" id="projects">
             <div className="projects-content">
                 <h2>Notable Projects</h2>
-                <a href='https://github.com/BLewis-Keyin/Flight-Data-Portal'>
-                    <div className="project" >
+                
+                <div className="project" ><a href='https://github.com/BLewis-Keyin/Flight-Data-Portal'>
                     <h3>Flight Data Portal</h3>
                     <p>
                         The Flight Data Portal is a web application designed to manage and display flight information. It features user authentication, API endpoints for flight data, and a modular routing system
                     </p>
-                    </div></a>
-                <a href='https://github.com/BLewis-Keyin/RedditVideoScraper'><div className="project" >
+                </a></div>
+                <div className="project" ><a href='https://github.com/BLewis-Keyin/RedditVideoScraper'>
                     <h3>Reddit Video Scraper</h3>
                     <p>
                         A python flask application that uses ffmpeg and the reddit API to serve mp4 video from a given reddit post URL
                     </p>
-                </div></a>
-            </div>
+                </a>
+            </div></div>
         </section>
     );
 }
