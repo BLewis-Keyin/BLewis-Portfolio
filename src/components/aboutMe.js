b96d31241213d2e088bc7ac8685eb3367994a895
BL-Keyin <you@example.com>
2024-03-13 06:37:47 -0400

    Protfolio Update 3/13/2024
    Modified projects section, About me section

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,8 +7,12 @@ function AboutMe() {
             <div className="about-me-content">
                 <h2>About Me</h2>
                 <p>
-                    Hello I'm a developer with a keen interest in backend development. I enjoy solving complex
+                    I'm a software engineer with a keen interest in backend development. I enjoy solving complex
                     problems and making things work.
+
+                    I am talented in making REST api's, working with Java, JavaScript and Python, and work well in agile environments.
+
+                    Currently seeking my first opportunity to break into the tech industry and join an awesome team and contribute to great projects.
                 </p>
             </div>
         </section>
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,19 +5,20 @@ function Projects() {
     return (
         <section className="projects" id="projects">
             <div className="projects-content">
-                <h2>Projects</h2>
-                <div className="project" href="https://github.com/BLewis-Keyin/Flight-Data-Portal">
+                <h2>Notable Projects</h2>
+                <a href='https://github.com/BLewis-Keyin/Flight-Data-Portal'>
+                    <div className="project" >
                     <h3>Flight Data Portal</h3>
                     <p>
-                        The Flight Data Portal is a web application designed to manage and display flight information. It features user authentication, API endpoints for flight data, and a modular routing system.
+                        The Flight Data Portal is a web application designed to manage and display flight information. It features user authentication, API endpoints for flight data, and a modular routing system
                     </p>
-                </div>
-                <div className="project" href="https://github.com/BLewis-Keyin/RedditVideoScraper">
+                    </div></a>
+                <a href='https://github.com/BLewis-Keyin/RedditVideoScraper'><div className="project" >
                     <h3>Reddit Video Scraper</h3>
                     <p>
-                        A python flask application that uses ffmpeg and the reddit API to serve mp4 video from a given reddit post URL.
+                        A python flask application that uses ffmpeg and the reddit API to serve mp4 video from a given reddit post URL
                     </p>
-                </div>
+                </div></a>
             </div>
         </section>
     );
